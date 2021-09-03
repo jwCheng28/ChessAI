a048421cd9bfcb82554ffd37f15e84944c4d6a31
jwCheng28 <jwCheng28@users.noreply.github.com>
2021-09-03 12:55:42 -0700

    Updated main for better testing

--- Program.cs
+++ Program.cs
@@ -8,9 +8,19 @@ namespace ChessAI
         static void Main(string[] args)
         {
             Board board = new Board();
-            board.DisplayBoard();
-            board.ValidMove(0, 16);
-            board.DisplayBoard();
+            board.DisplayFEN();
+            int start, end;
+            for (int i = 0; i < 5; ++i)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Enter piece location");
+                start = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter target location");
+                end = Convert.ToInt32(Console.ReadLine());
+                board.ValidMove(start, end);
+                Console.WriteLine("");
+                board.DisplayFEN();
+            }
         }
     }
 }
