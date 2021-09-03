7264123f89e2bd591cfb939e9beea207ac386adc
jwCheng28 <jwCheng28@users.noreply.github.com>
2021-09-03 12:56:00 -0700

    Added board to FEN conversion and FEN display

--- ChessGame.cs
+++ ChessGame.cs
@@ -9,6 +9,14 @@ namespace ChessGame
     {
         private List<ChessPiece> board;
         private string currentPositionFEN;
+        private static readonly Hashtable pieceTranslation = new Hashtable() {
+                {'p', PieceAttributes.Pawn}, {'n', PieceAttributes.Knight}, {'b', PieceAttributes.Bishop},
+                {'r', PieceAttributes.Rook}, {'q', PieceAttributes.Queen}, {'k', PieceAttributes.King}
+        };
+        private static readonly Hashtable pieceReverseTranslation = new Hashtable() {
+            {PieceAttributes.Pawn, 'p'}, {PieceAttributes.Knight, 'n'}, {PieceAttributes.Bishop, 'b'},
+            {PieceAttributes.Rook, 'r'}, {PieceAttributes.Queen, 'q'}, {PieceAttributes.King, 'k'}
+        };
 
         public Board() : this("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
         {
@@ -18,7 +26,6 @@ namespace ChessGame
         {
             currentPositionFEN = FEN;
             board = new List<ChessPiece>(64);
-            int currentPosition = 0;
             foreach (char piece in FEN)
             {
                 if (piece.Equals('/'))
@@ -44,40 +51,101 @@ namespace ChessGame
         {
             ChessPiece chessPiece = new ChessPiece();
             chessPiece.pieceColor = Char.IsUpper(piece) ? PieceAttributes.White : PieceAttributes.Black;
-            Hashtable pieceTranslation = new Hashtable() {
-                {'p', PieceAttributes.Pawn}, {'n', PieceAttributes.Knight}, {'b', PieceAttributes.Bishop},
-                {'r', PieceAttributes.Rook}, {'q', PieceAttributes.Queen}, {'k', PieceAttributes.King}
-            };
             chessPiece.pieceRank = (int) pieceTranslation[Char.ToLower(piece)];
             return chessPiece;
         }
 
+        private void UpdateFEN()
+        {
+            List<char> FEN = new List<char>();
+            int spaceCount = 0;
+            for (int i = 0; i < 64; ++i)
+            {
+                if (i != 0 && i%8 == 0)
+                {
+                    if (spaceCount > 0)
+                    {
+                        FEN.Add((char) ('0' + spaceCount));
+                    }
+                    FEN.Add('/');
+                    spaceCount = 0;
+                }
+                if (board[i].pieceRank == PieceAttributes.Empty)
+                {
+                    spaceCount++;
+                } 
+                else 
+                {
+                    if (spaceCount > 0)
+                    {
+                        FEN.Add((char) ('0' + spaceCount));
+                    }
+                    char piece = (char) pieceReverseTranslation[board[i].pieceRank];
+                    if (board[i].pieceColor == PieceAttributes.White)
+                    {
+                        piece = Char.ToUpper(piece);
+                    }
+                    FEN.Add(piece);
+                    spaceCount = 0;
+                }
+            }
+            currentPositionFEN = string.Join("", FEN);
+        }
+
         public void DisplayBoard()
         {
-            for (int i = 0; i < 64; i++) 
+            Console.WriteLine("\n------------------------");
+            for (int i = 0; i < 64; ++i) 
             {
                 if (i%8 == 0)
                 {
                     Console.WriteLine("");
                 }
-                Console.Write($"{board[i].pieceRank} ");
+                Console.Write($" {board[i].pieceRank} ");
             }
+            Console.WriteLine("\n------------------------");
+        }
+
+        public void DisplayFEN()
+        {
+            Console.WriteLine("\n------------------------");
+            foreach (char piece in currentPositionFEN)
+            {
+                if (piece.Equals('/'))
+                {
+                    Console.WriteLine("");
+                }
+                else if (Char.IsDigit(piece))
+                {
+                    int spaces = piece - '0';
+                    for (int i = 0; i < spaces; ++i)
+                    {
+                        Console.Write("   ");
+                    }
+                }
+                else
+                {
+                    Console.Write(" " + piece + " ");
+                }
+            }
+            Console.WriteLine("\n------------------------");
         }
 
         public bool ValidMove(int piecePosition, int targetPosition)
         {
             if (board[piecePosition].pieceRank == PieceAttributes.Empty)
             {
-                Console.Write("There's no piece at that position");
+                Console.WriteLine("There's no piece at that position");
                 return false;
             }
             if (PieceMovement.ValidMove(board[piecePosition], piecePosition, targetPosition))
             {
-                Console.Write("Valid Move");
                 board[targetPosition] = board[piecePosition];
                 board[piecePosition] = new ChessPiece(PieceAttributes.Empty, PieceAttributes.Empty);
+                UpdateFEN();
                 return true;
             }
+            Console.WriteLine("Move Invalid");
             return false;
         }
 
